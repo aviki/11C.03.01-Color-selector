ef281ceec317eb694799b05729939c6878b3a6df
aviki <toldi.victoria@gmail.com>
2020-02-19 23:59:55 +0100

    Implemented hex to rgb conversion

--- colorselect.js
+++ colorselect.js
@@ -10,3 +10,16 @@ var hexCode = theInput.value;
 theInput.addEventListener("input", function displayHEX() {
   document.getElementById("hex-input").innerHTML = theInput.value;
 });
+
+//Converting HEX to RGB --> Parameter is a string, output is an array of three integers
+function hexToRgb(hex) {
+  let rHex = hex.substr(1, 2);
+  let gHex = hex.substr(3, 2);
+  let bHex = hex.substr(5, 2);
+  let r = parseInt(rHex, 16);
+  let g = parseInt(gHex, 16);
+  let b = parseInt(bHex, 16);
+  return [r, g, b];
+}
+
+//Converting RGB to HSL
