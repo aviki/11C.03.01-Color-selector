a4c85742af489c103dbba7ed72501c39da564849
aviki <toldi.victoria@gmail.com>
2020-02-19 19:29:55 +0100

    Displayed HEX code with a function

--- colorselect.js
+++ colorselect.js
@@ -3,3 +3,10 @@
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {}
+
+//Get the value of the color from the input field
+var theInput = document.getElementById("main-color");
+var hexCode = theInput.value;
+theInput.addEventListener("input", function displayHEX() {
+  document.getElementById("hex-input").innerHTML = theInput.value;
+});
